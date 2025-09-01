ae9ad53ba82d2d9c9ab2637bea9c970b7c98dc75
Sung-Hun Kim <p777737@gmail.com>
2025-09-01 16:36:17 +0900

    new period notice added

---
+++ src/components/NextPeriodNotice.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Button from "./_UI/ZonkButton.svelte";
+
+  interface Props {
+    onGoNext: () => void;
+  }
+
+  let {
+    onGoNext
+  }: Props = $props();
+
+  const handleClick = () => {
+    onGoNext();
+  }
+
+</script>
+
+<div class="w-full bg-yellow-300 p-2 text-center">
+  <h2>Period has ended.</h2>
+  <Button onclick={handleClick}>Next</Button>
+</div>
